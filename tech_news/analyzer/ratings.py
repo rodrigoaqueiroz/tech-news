58f286abfa32a7a9bd774dba62d7b7afa140baa3
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-04-10 21:54:08 -0300

    feat: req11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -13,15 +13,20 @@ def top_5_news():
         {"$sort": {"rank_5": -1, "title": 1}},
         {"$limit": 5},
     ])
-    ranked = [
+    best_ranked_news = [
         (best_ranked["title"], best_ranked["url"]) for best_ranked in news
     ]
-    return ranked
+    return best_ranked_news
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
-
-
-print(top_5_news())
+    categories = get_collection().aggregate([
+        {"$unwind": "$categories"},
+        {"$sort": {"categories": 1}},
+        {"$limit": 5},
+    ])
+    best_ranked_categories = [
+        best_ranked["categories"] for best_ranked in categories
+    ]
+    return best_ranked_categories
